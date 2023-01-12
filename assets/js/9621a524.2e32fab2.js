6964599f66a5f53cc9660686b2a0f80f91ce0ccb
Matija Sosic <matija.sosic@gmail.com>
2023-01-12 17:02:18 +0100

    Deploy website - based on afd59877ca358583b225a88ec7d507b51b48e734

--- assets/js/9621a524.2e32fab2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[2422],{49109:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/webdev/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":20,"previousPage":"/blog/tags/webdev","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
