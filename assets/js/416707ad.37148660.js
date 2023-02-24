53ba413e639eec7513e96ff4d1e35b8a2c83ef76
vincanger <70215737+vincanger@users.noreply.github.com>
2023-02-24 12:26:31 +0100

    Deploy website - based on 2ae547132d2bbafd2b04162193797d573ace6d02

---
+++ assets/js/416707ad.37148660.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[1114],{49108:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/web-dev","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
