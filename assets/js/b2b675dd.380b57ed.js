5d347cfbd2a2c437ac7efef5a1f92f957d72008f
vincanger <70215737+vincanger@users.noreply.github.com>
2023-09-17 14:35:40 +0200

    Deploy website - based on 51c9704b82b7902be35b80cefa8a2f657f4315e0

--- assets/js/b2b675dd.380b57ed.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[533],{28017:e=>{e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":61,"totalPages":1,"totalCount":61,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
